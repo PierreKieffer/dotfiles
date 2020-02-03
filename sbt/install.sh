c768ade75a0fc6e167beaf147b859a72607fc7df
Pierre <38128948+PierreKieffer@users.noreply.github.com>
2020-02-03 09:55:48 +0100

    Add files via upload

---
+++ sbt/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+sudo apt-get update
+sudo apt-get install -y \
+	zip \
+	unzip
+wget https://piccolo.link/sbt-0.13.15.zip
+mv sbt-0.13.15.zip /home/$USER 
+unzip /home/$USER/sbt-0.13.15.zip -d /home/$USER
+rm /home/$USER/sbt-0.13.15.zip
+
+echo 'export PATH=$PATH:/home/$USER/sbt/bin' >> /home/$USER/.bashrc
+source .bashrc 
+
+
---
+++ vim/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+# install vim-plug 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# copy .vimrc file to home dir 
+cp $PWD/.vimrc /home/$USER/.vimrc 
