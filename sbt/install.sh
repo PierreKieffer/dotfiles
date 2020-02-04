d2de38db97a15ec173529fe9799d168c80ab5293
Pierre <38128948+PierreKieffer@users.noreply.github.com>
2020-02-04 14:41:02 +0100

    Update install.sh

--- sbt/install.sh
+++ sbt/install.sh
@@ -2,15 +2,13 @@
 
 sudo apt-get update
 sudo apt-get install -y \
-	zip \
-	unzip
-wget https://piccolo.link/sbt-0.13.15.zip
-mv sbt-0.13.15.zip /home/$USER 
-unzip /home/$USER/sbt-0.13.15.zip -d /home/$USER
-rm /home/$USER/sbt-0.13.15.zip
+        zip \
+        unzip
+wget https://piccolo.link/sbt-1.3.7.zip
+mv sbt-1.3.7.zip /home/$USER
+unzip /home/$USER/sbt-1.3.7.zip -d /home/$USER
+rm /home/$USER/sbt-1.3.7.zip
 
 echo 'export PATH=$PATH:/home/$USER/sbt/bin' >> /home/$USER/.bashrc
-echo 'export TERM=xterm-color' >> /home/$USER/.bashrc
+
 source /home/$USER/.bashrc
-
-
