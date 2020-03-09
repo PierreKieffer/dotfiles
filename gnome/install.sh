e7970579642ac4f569962a480d4678aa22f86735
Pierre <38128948+PierreKieffer@users.noreply.github.com>
2020-03-09 15:05:56 +0100

    Add files via upload

---
+++ gnome/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+sudo apt update
+sudo apt install gnome-tweaks 
+
+sudo rm -rf /usr/share/themes/{Arc,Arc-Darker,Arc-Dark}
+rm -rf ~/.local/share/themes/{Arc,Arc-Darker,Arc-Dark}
+rm -rf ~/.themes/{Arc,Arc-Darker,Arc-Dark}
+
+sudo add-apt-repository ppa:noobslab/themes
+sudo apt-get update
+sudo apt-get install arc-theme
+
+sudo add-apt-repository ppa:noobslab/icons
+sudo apt-get update
+sudo apt-get install arc-icons
+
+sudo apt install gnome-shell-extensions
+
+
+
+
