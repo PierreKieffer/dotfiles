93e826ec4b1b1c28aa008d58c52dee4bbe2e2410
Pierre <38128948+PierreKieffer@users.noreply.github.com>
2020-03-09 15:37:53 +0100

    init dev

---
+++ dev/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+
+sudo apt install scala 
+sudo apt install openjdk-8-jre-headless 
+wget -P ~/Downloads https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
+bash ~/Downloads/Anaconda3-2019.10-Linux-x86_64.sh
+
+
+
