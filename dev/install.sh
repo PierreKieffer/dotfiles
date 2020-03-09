39f694d9401511a20254ee421c1e3d68cfb64bf3
Pierre <38128948+PierreKieffer@users.noreply.github.com>
2020-03-09 18:03:36 +0100

    Update install.sh

--- dev/install.sh
+++ dev/install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash 
 
-
+sudo apt update 
+sudo apt install vim 
+sudo apt install git 
+sudo apt install curl 
 sudo apt install scala 
 sudo apt install golang-go
 sudo apt install openjdk-8-jre-headless 
 wget -P ~/Downloads https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
 bash ~/Downloads/Anaconda3-2019.10-Linux-x86_64.sh
-
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 sudo apt-get install apt-transport-https ca-certificates gnupg
 sudo apt-get update && sudo apt-get install google-cloud-sdk
