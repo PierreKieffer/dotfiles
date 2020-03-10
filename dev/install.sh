77940e5d84ddc67a5241c3302c02969f14c4b21b
Pierre <pierre.kieffer@hubstairs.com>
2020-03-10 09:22:42 +0100

    Update install scripting

--- dev/install.sh
+++ dev/install.sh
@@ -8,6 +8,10 @@ sudo apt update \
 	&& apt install -y golang-go \
 	&& apt install -y openjdk-8-jre-headless \
 	&& apt install -y  mongodb-clients
+
+# go get github.com/tkanos/gonfig
+# go get go.mongodb.org/mongo-driver/mongo
+
 wget -P ~/Downloads https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
 bash ~/Downloads/Anaconda3-2019.10-Linux-x86_64.sh
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
