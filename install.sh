2d36bb3204ca1ce3236234534ab38c4d9f838e79
Pierre <pierre.kieffer@hubstairs.com>
2020-03-10 08:53:19 +0100

    Update install scripting

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 chmod 777 -R $PWD/*
 
-# $PWD/terminator/install.sh
-# $PWD/dev/install.sh
-# $PWD/vim/install.sh
-# $PWD/gnome/install.sh
-# $PWD/docker/install.sh
+$PWD/terminator/install.sh
+$PWD/dev/install.sh
+$PWD/vim/install.sh
+$PWD/gnome/install.sh
+$PWD/docker/install.sh
 
