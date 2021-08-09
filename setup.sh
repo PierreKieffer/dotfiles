55d53e576ac5d543e908f7ff78156305f0dde684
Pierre <pierre.kieffer@hubstairs.com>
2021-08-09 10:59:04 +0200

    update setup

--- setup.sh
+++ setup.sh
@@ -101,5 +101,5 @@ echo 'gc() {
 
 
 # prompt
-# PS1="${debian_chroot:+($debian_chroot)}>\[\033[01;34m\]\w\[\033[00m\]\[\033[01;33m\]\$(__git_ps1 ' [%s] ')\[\033[00m\]\$ "
+# PS1="${debian_chroot:+($debian_chroot)}> \[\033[01;34m\]\w\[\033[00m\]\[\033[01;33m\]\$(__git_ps1 ' [%s] ')\[\033[00m\]\$ "
 
